cebdbd5651f85152ec0e4e97a4e01e66311fb773
NageshMaddala <nageshvmr@gmail.com>
2025-01-23 15:59:01 -0500

    excercises

--- Sql Snippets/window-functions.sql
+++ Sql Snippets/window-functions.sql
@@ -46,3 +46,76 @@ Notes:
 2. The aggregate calcuation is simple tacked on to the query as an additional column
 3. It is also possible to group these calculations, just like we can with aggregate queries, using partition
 */
+
+--Exercise 1:
+
+SELECT 
+ B.FirstName,
+ B.LastName,
+ C.JobTitle,
+ A.Rate,
+ AverageRate = AVG(A.Rate) OVER()
+
+FROM AdventureWorks2019.HumanResources.EmployeePayHistory A
+	JOIN AdventureWorks2019.Person.Person B
+		ON A.BusinessEntityID = B.BusinessEntityID
+	JOIN AdventureWorks2019.HumanResources.Employee C
+		ON A.BusinessEntityID = C.BusinessEntityID
+
+
+
+--Exercise 2:
+
+SELECT 
+ B.FirstName,
+ B.LastName,
+ C.JobTitle,
+ A.Rate,
+ AverageRate = AVG(A.Rate) OVER(),
+ MaximumRate = MAX(A.Rate) OVER()
+
+FROM AdventureWorks2019.HumanResources.EmployeePayHistory A
+	JOIN AdventureWorks2019.Person.Person B
+		ON A.BusinessEntityID = B.BusinessEntityID
+	JOIN AdventureWorks2019.HumanResources.Employee C
+		ON A.BusinessEntityID = C.BusinessEntityID
+
+
+
+
+--Exercise 3:
+
+SELECT 
+ B.FirstName,
+ B.LastName,
+ C.JobTitle,
+ A.Rate,
+ AverageRate = AVG(A.Rate) OVER(),
+ MaximumRate = MAX(A.Rate) OVER(),
+ DiffFromAvgRate = A.Rate - AVG(A.Rate) OVER()
+
+FROM AdventureWorks2019.HumanResources.EmployeePayHistory A
+	JOIN AdventureWorks2019.Person.Person B
+		ON A.BusinessEntityID = B.BusinessEntityID
+	JOIN AdventureWorks2019.HumanResources.Employee C
+		ON A.BusinessEntityID = C.BusinessEntityID
+
+
+
+--Exercise 4:
+
+SELECT 
+ B.FirstName,
+ B.LastName,
+ C.JobTitle,
+ A.Rate,
+ AverageRate = AVG(A.Rate) OVER(),
+ MaximumRate = MAX(A.Rate) OVER(),
+ DiffFromAvgRate = A.Rate - AVG(A.Rate) OVER(),
+ PercentofMaxRate = (A.Rate / MAX(A.Rate) OVER()) * 100
+
+FROM AdventureWorks2019.HumanResources.EmployeePayHistory A
+	JOIN AdventureWorks2019.Person.Person B
+		ON A.BusinessEntityID = B.BusinessEntityID
+	JOIN AdventureWorks2019.HumanResources.Employee C
+		ON A.BusinessEntityID = C.BusinessEntityID
